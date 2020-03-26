620b86a9634a21444627c9f89e0982be39c7eddf
jemaiAH <ahmed.jemai@hotmail.com>
2020-03-26 15:13:32 +0100

    create the search bar, table and pagination on the page categories

--- Frontend/src/app/category/category/category.component.ts
+++ Frontend/src/app/category/category/category.component.ts
@@ -10,6 +10,37 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class CategoryComponent implements OnInit {
 
+  length: number; // nombre de ligne  du tableau
+  pageSize = 4; // nombre de ligne maximal par page
+  displayedColumns: string[] = ['Nom', 'Type', 'Photo', 'Action']; // les colonnes du tableau
+  dataSource = new MatTableDataSource([]); // Data du tableau
+  input = [
+    {Nom: 'sport', Type: 'personnel', Photo: 'atarafaga'},
+    {Nom: 'sport', Type: 'personnel', Photo: 'atarafaga'},
+    {Nom: 'sport', Type: 'personnel', Photo: 'atarafaga'},
+    {Nom: 'sport', Type: 'personnel', Photo: 'atarafaga'},
+    {Nom: 'sport', Type: 'personnel', Photo: 'atarafaga'}
+  ];
+  private paginator: MatPaginator;
+  private sort: any;
+  @ViewChild(MatPaginator, { static: false }) set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+    this.dataSource.paginator = this.paginator;
+  } // pagination
+  @ViewChild(MatSort, { static: true }) set content(content: ElementRef) {
+    this.sort = content;
+    this.dataSource.sort = this.sort;
+  } // tri
+  constructor() {
+    this.length = this.input.length; // Affectation du nombre de ligne du tableau
+    // tslint:disable-next-line: prefer-for-of
+    for (let i = 0; i < this.input.length; i++) {
+       // tslint:disable-next-line: no-string-literal
+       this.input[i]['Action'] = ' '; // ajout de l'espace entre les deux boutons
+    }
+    this.dataSource = new MatTableDataSource(this.input); // Remplissage du tableau par les données
+  }
+
   ngOnInit(): void {
   }
 
