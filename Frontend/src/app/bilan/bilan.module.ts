983055f2d0878ce178351a580a82a90ac477ca5d
jemaiAH <ahmed.jemai@hotmail.com>
2020-04-05 16:04:40 +0200

    create cards for showing lists and diagram of statistics on the page Bilan

--- Frontend/src/app/bilan/bilan.module.ts
+++ Frontend/src/app/bilan/bilan.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BilanComponent } from './bilan/bilan.component';
-
-
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { ChartsModule } from 'ng2-charts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [BilanComponent],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    ChartsModule,
+    NgbModule
+  ],
+  exports: [MatTableModule, MatPaginatorModule, MatSortModule, MatFormFieldModule, MatInputModule]
 })
 export class BilanModule { }
--- Frontend/src/app/bilan/bilan/bilan.component.ts
+++ Frontend/src/app/bilan/bilan/bilan.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ListService } from 'src/app/services/list.service';
+import { MatDialog } from '@angular/material/dialog';
+import { List } from 'src/app/models/list.model';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { ChartOptions, ChartType, ChartDataSets } from 'chart.js';
+import { Label } from 'ng2-charts';
 
 @Component({
   selector: 'app-bilan',
@@ -6,10 +14,62 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bilan.component.css']
 })
 export class BilanComponent implements OnInit {
+  barChartOptions: ChartOptions = {
+    responsive: true,
+  };
+  barChartLabels: Label[] = ['Apple', 'Banana', 'Kiwifruit', 'Blueberry', 'Orange', 'Grapes'];
+  barChartType: ChartType = 'bar';
+  barChartLegend = true;
+  barChartPlugins = [];
 
-  constructor() { }
+  barChartData: ChartDataSets[] = [
+    { data: [45, 37, 60, 70, 46, 33], label: 'Best Fruits' }
+  ];
+  length: number; // nombre de ligne  du tableau
+  pageSize = 4; // nombre de ligne maximal par page
+  displayedColumns: string[] = ['Nom', 'Type', 'Category', 'DateDebut',
+    'DateFin', 'Percent', 'IsLate', 'Action']; // les colonnes du tableau
+  dataSource = new MatTableDataSource([]); // Data du tableau
+
+  private paginator: MatPaginator;
+  private sort: any;
+  @ViewChild(MatPaginator, { static: false }) set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+    this.dataSource.paginator = this.paginator;
+  } // pagination
+  @ViewChild(MatSort, { static: false }) set content(content: ElementRef) {
+    this.sort = content;
+    this.dataSource.sort = this.sort;
+  } // tri
+
+  constructor(private listService: ListService,
+              private matDialog: MatDialog) { }
 
   ngOnInit(): void {
   }
+
+ updateData() {
+  const newLists: List[] = [];
+  for (let i = 0; i < this.listService.lists.length; i++) {
+      this.listService.changeToLateList(this.listService.lists[i]);
+      newLists.push(this.listService.lists[i]);
+  }
+  for (let j = 0; j < newLists.length; j++) {
+    // tslint:disable-next-line: no-string-literal
+    newLists[j]['Action'] = j; // ajout de l'espace entre les deux boutons
+    newLists[j]['Checkbox'] = '';
+  }
+  this.dataSource = new MatTableDataSource(newLists); // Remplissage du tableau par les données
+  this.length = newLists.length; // Affectation du nombre de ligne du tableau
+  // tslint:disable-next-line: prefer-for-of
+}
+
+deleteList(list) {
+
+}
+
+detailsList(list) {
+
+}
 
 }
