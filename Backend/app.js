7b867dfd8c442bcffff08ecbaea87e2f24cd07a0
jemaiAH <ahmed.jemai@hotmail.com>
2020-04-12 12:56:59 +0200

    create the middleware of multer configuration

--- Backend/app.js
+++ Backend/app.js
@@ -1,3 +1,4 @@
+/********** importation des librairies et des frameworks ***********************************/
 const express = require('express'); // importation de micro-framework express
 const db = require("./models/index"); // importation de framework sequelize
 const bodyparser = require('body-parser'); // importation de package permettant de transformer le corps de la requete en objet json
@@ -6,10 +7,11 @@ const List = require('./models/list.model'); // importation du modèle list
 const Category = require('./models/category.model'); // importation du modèle catégorie
 const User = require('./models/user.model'); // importation du modéle utilisateur
 const app = express(); // istanciation de l'objet express afin de créer le serveur 
-// connexion à la base de données
+
+/************************ connexion à la base de données ***********************************/
 db.sync();
 
-// test de la connexion
+/****************************** test de la connexion ****************************************/ 
 db
   .authenticate()
   .then(() => {
@@ -19,7 +21,7 @@ db
     console.error('Unable to connect to the database:', err);
   });
 
-// création des middlewares pour plusieurs fonctionnalités
+/****************** création des middlewares pour plusieurs fonctionnalités ******************/  
 // middleware pour la sécurité
 app.use(cors()); 
 app.options('*', cors());
@@ -27,7 +29,7 @@ app.options('*', cors());
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ limit: '10mb', extended: true }));
 
-// API REST
+/****************************************** API REST *****************************************/   
 app.post('/api/category', (req, resp, next) => {
     const category = new Category({
         Nom: req.body.Nom,
@@ -37,7 +39,9 @@ app.post('/api/category', (req, resp, next) => {
     category.save()
     .then((data)=> {resp.status(201).json({message: 'la catégorie est ajoutée avec succès !', data: data})})
     .catch((err) => {resp.status(400).json("l'erreur est la suivante: ", err)});
-}); // middleware pour traiter la requeste et la réponse associées à la route '/api/category'
+}); // middleware pour traiter la requeste et la réponse associées à la route post '/api/category'
+
+
 
 
 module.exports = app;
