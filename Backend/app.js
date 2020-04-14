c7fbd71af8ebad4afa4f3bc6ae0dde7a22ee1d50
jemaiAH <ahmed.jemai@hotmail.com>
2020-04-14 23:21:34 +0200

    add middlewares for download images and categories

--- Backend/app.js
+++ Backend/app.js
@@ -8,9 +8,10 @@ const Category = require('./models/category.model'); // importation du modèle c
 const User = require('./models/user.model'); // importation du modéle utilisateur
 const app = express(); // istanciation de l'objet express afin de créer le serveur 
 const multer = require('./config/multer.config'); // importation du middleware de configuration de multer
+const path = require('path');
 
 /************************ connexion à la base de données ***********************************/
-db.sync({force: true});
+db.sync({force: false}); // warning: if force is true, the database will be omitted ! please, take care !!! 
 
 /****************************** test de la connexion ****************************************/ 
 db
@@ -42,8 +43,15 @@ app.post('/api/category', multer, (req, resp, next) => {
     category.save()
     .then((data)=> {resp.status(201).json({message: 'la catégorie est ajoutée avec succès !', data: data})})
     .catch((err) => {resp.status(400).json("l'erreur est la suivante: ", err)});
-}); // middleware pour traiter la requeste et la réponse associées à la route post '/api/category'
+}); // middleware pour traiter la requete et la réponse associées à la route post '/api/category'
 
+app.use('/images', express.static(path.join(__dirname, 'images'))); // middleware pour télécharger l'image du serveur
+
+app.get('/api/category', (req, res, next) => {
+    Category.findAll()
+    .then((categories) => { res.status(200).json({ message: 'les catégories sont téléchargées avec succès !', Data: categories }) })
+    .catch((err) => { res.status(400).json({ err }) });
+}); // middleware pour traiter la requeste et la réponse associées à la route get '/api/category'
 
 
 
